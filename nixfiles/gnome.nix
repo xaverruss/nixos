ffd3a2bfcb341f869ac0aa842b03247557781369
Xaver Russ <me@xaverruss.de>
2023-09-12 21:38:18 +0200

    removed unwanted gnome stuff

--- nixfiles/gnome.nix
+++ nixfiles/gnome.nix
@@ -17,14 +17,27 @@
   # networking.firewall.allowedTCPPorts = [ 3389 ];
   services.gnome.gnome-remote-desktop.enable = true;
 
-  services.gnome = {
-    tracker.enable = true;
-    sushi.enable = true;
-    gnome-user-share.enable = true;
-  };
+  environment.gnome.excludePackages =
+    (with pkgs; [ gnome-photos gnome-tour gnome-text-editor ])
+    ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      # gnome-terminal
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      gnome-calculator
+      yelp # help viewer
+      gnome-maps
+      gnome-weather
+      gnome-contacts
+      simple-scan
+    ]);
 
-  environment.systemPackages = with pkgs; [
-    gnomeExtensions.gsconnect
-    gnomeExtensions.wifi-qrcode
-  ];
 }
