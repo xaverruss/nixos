6a01aadbcc16787d7ff02c4a7f710b860eeac26b
Xaver Russ <me@xaverruss.de>
2023-09-12 21:32:35 +0200

    added firefox to flatpak cuz you maybe need one

--- nixfiles/flatpak.nix
+++ nixfiles/flatpak.nix
@@ -7,4 +7,6 @@
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
+  # Add firefox, ya need a browser dont ya ? 
+  environment.systemPackages = with pkgs; [ firefox ];
 }
