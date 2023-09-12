8cf42f5bf4949f8b0fca244b8ed1fadbcae17ea1
Xaver Russ <me@xaverruss.de>
2023-09-12 17:29:18 +0200

    stuff

--- setup.sh
+++ setup.sh
@@ -40,6 +40,7 @@ echo "Double check if DISKS have been replaced correctly."
 echo " !!!!!!!"
 echo "Double check the 2. install script if partitions are correctly set"
 
+
 echo "This system currently uses:"
 if [ -d "/sys/firmware/efi" ]; then
   echo "UEFI mode"
