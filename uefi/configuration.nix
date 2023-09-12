83be406e7801519959b5540c8e567d86056825c2
Xaver Russ <me@xaverruss.de>
2023-09-12 21:06:28 +0200

    add some special usecases

--- uefi/configuration.nix
+++ uefi/configuration.nix
@@ -15,4 +15,32 @@
     };
     efi.canTouchEfiVariables = true;
   };
+
+  # Use this if it is some weird Laptop / Intel Compute stick
+  # boot.loader = {
+  #   efi = { canTouchEfiVariables = false; };
+  #   grub = {
+  #     efiSupport = true;
+  #     efiInstallAsRemovable = true;
+  #     device = "nodev";
+  #   };
+  # };
+
+  # Use this if you need to store kernel and init.d on / 
+  # Happens if you do not have enoth space on /boot, like Oracle VPS
+  # Change following line in hardware-configuration.nix
+  # fileSystems."/boot/efi" = ... # ← mount your ESP here instead of at /boot/.
+
+  # boot.loader = {
+  #   efi = {
+  #     canTouchEfiVariables = true;
+  #     efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+  #   };
+  #   grub = {
+  #      efiSupport = true;
+  #      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+  #      device = "nodev";
+  #   };
+  # };
+
 }
