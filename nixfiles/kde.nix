0836a6dd4b44ec9531abcb2969261b56c2781d51
Xaver Russ <me@xaverruss.de>
2023-09-12 21:30:34 +0200

    network manager on kde

--- nixfiles/kde.nix
+++ nixfiles/kde.nix
@@ -3,7 +3,13 @@
 {
   imports = [ ./flatpak.nix ];
 
+  # Enable network manager
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
