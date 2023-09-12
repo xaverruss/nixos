13b71d57ab2fd095f8cc3cd7d0e5bc03f93fddd6
Xaver Russ <me@xaverruss.de>
2023-09-12 23:12:16 +0200

    fixed typo

--- nixfiles/hyprland.nix
+++ nixfiles/hyprland.nix
@@ -71,62 +71,62 @@
       extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
       setLdLibraryPath = true;
     };
+  };
 
-    environment.systemPackages = with pkgs; [
-      kitty
-      polkit_gnome
-      libva-utils
-      fuseiso
-      udiskie
-      gnome.adwaita-icon-theme
-      gnome.gnome-themes-extra
-      # nvidia-vaapi-driver
-      gsettings-desktop-schemas
-      swaynotificationcenter
-      wlr-randr
-      ydotool
-      hyprland-share-picker
-      wl-clipboard
-      hyprland-protocols
-      hyprpicker
-      swayidle
-      swaylock
-      xdg-desktop-portal-hyprland
-      hyprpaper
-      wofi
-      firefox-wayland
-      swww
-      grim
-      xdg-utils
-      xdg-desktop-portal
-      xdg-desktop-portal-gtk
-      qt5.qtwayland
-      qt6.qmake
-      qt6.qtwayland
-      adwaita-qt
-      adwaita-qt6
-    ];
+  environment.systemPackages = with pkgs; [
+    kitty
+    polkit_gnome
+    libva-utils
+    fuseiso
+    udiskie
+    gnome.adwaita-icon-theme
+    gnome.gnome-themes-extra
+    # nvidia-vaapi-driver
+    gsettings-desktop-schemas
+    swaynotificationcenter
+    wlr-randr
+    ydotool
+    hyprland-share-picker
+    wl-clipboard
+    hyprland-protocols
+    hyprpicker
+    swayidle
+    swaylock
+    xdg-desktop-portal-hyprland
+    hyprpaper
+    wofi
+    firefox-wayland
+    swww
+    grim
+    xdg-utils
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    qt5.qtwayland
+    qt6.qmake
+    qt6.qtwayland
+    adwaita-qt
+    adwaita-qt6
+  ];
 
-    # environment.sessionVariables = {
-    #   POLKIT_AUTH_AGENT =
-    #     "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    #   GSETTINGS_SCHEMA_DIR =
-    #     "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
-    #   # LIBVA_DRIVER_NAME = "nvidia";
-    #   XDG_SESSION_TYPE = "wayland";
-    #   # GBM_BACKEND = "nvidia-drm";
-    #   # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    #   WLR_NO_HARDWARE_CURSORS = "1";
-    #   NIXOS_OZONE_WL = "1";
-    #   MOZ_ENABLE_WAYLAND = "1";
-    #   SDL_VIDEODRIVER = "wayland";
-    #   _JAVA_AWT_WM_NONREPARENTING = "1";
-    #   CLUTTER_BACKEND = "wayland";
-    #   WLR_RENDERER = "vulkan";
-    #   XDG_CURRENT_DESKTOP = "Hyprland";
-    #   XDG_SESSION_DESKTOP = "Hyprland";
-    #   GTK_USE_PORTAL = "1";
-    #   NIXOS_XDG_OPEN_USE_PORTAL = "1";
-    # };
+  environment.sessionVariables = {
+    POLKIT_AUTH_AGENT =
+      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    GSETTINGS_SCHEMA_DIR =
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+    # LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    # GBM_BACKEND = "nvidia-drm";
+    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    SDL_VIDEODRIVER = "wayland";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    CLUTTER_BACKEND = "wayland";
+    WLR_RENDERER = "vulkan";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    GTK_USE_PORTAL = "1";
+    NIXOS_XDG_OPEN_USE_PORTAL = "1";
   };
 }
