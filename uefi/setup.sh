c10d433af768f0a864831c92d73222a9bb9e3438
Xaver Russ <me@xaverruss.de>
2023-09-12 17:11:52 +0200

    changed order

--- uefi/setup.sh
+++ uefi/setup.sh
@@ -2,13 +2,14 @@
 
 # Read HOSTNAME
 read -p "Enter Hostname for this system value: " hostname
-# Replace "HOSTNAME" in configuration.nix
-sed "s|HOSTNAME|${hostname}|g" "../install.nix" > "../install.nix.tmp"
-mv ../install.nix.tmp ../install.nix.nix
-
 
 # Prompt the user for the /dev/DISK value
 read -p "Enter /dev/DISK value: " disk_value
+
+
+# Replace "HOSTNAME" in configuration.nix
+sed "s|HOSTNAME|${hostname}|g" "../install.nix" > "../install.nix.tmp"
+mv ../install.nix.tmp ../install.nix.nix
 
 # Replace "DISK" in script 1
 sed "s|/dev/DISK|/dev/${disk_value}|g" "1-install.sh" > "1-install.sh.tmp"
