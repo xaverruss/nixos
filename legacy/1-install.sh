03001d04d948f83ea3b178b382798807666eca12
Xaver Russ <me@xaverruss.de>
2023-09-12 17:35:37 +0200

    added notes

--- legacy/1-install.sh
+++ legacy/1-install.sh
@@ -5,3 +5,7 @@ echo "Replace DISK with the disk you wanna use"
 parted /dev/DISK -- mklabel msdos
 parted /dev/DISK -- mkpart primary 1MB 100%
 
+echo "Double check the 2. install script if partitions are correctly set"
+echo " "
+lsblk 
+echo " "
--- uefi/1-install.sh
+++ uefi/1-install.sh
@@ -4,5 +4,7 @@ parted /dev/DISK -- mkpart primary 512MB 100%
 parted /dev/DISK -- mkpart ESP fat32 1MB 512MB
 parted /dev/DISK -- set 2 esp on
 
-
-
+echo "Double check the 2. install script if partitions are correctly set"
+echo " "
+lsblk 
+echo " "
