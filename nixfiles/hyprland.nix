f529aed4fa334a12c6a70d026e19725531deb287
Xaver Russ <me@xaverruss.de>
2023-09-13 02:27:05 +0200

    more hyprland

--- nixfiles/hyprland.nix
+++ nixfiles/hyprland.nix
@@ -10,6 +10,7 @@
       displayManager.sddm = {
         enable = true;
         defaultSession = "Hyprland";
+        settings = { General = { DisplayServer = "wayland"; }; };
       };
     };
   };
@@ -39,15 +40,6 @@
   #   };
   # };
 
-  # Security
-  # security = {
-  #   pam.services.swaylock = {
-  #     text = ''
-  #       auth include login
-  #     '';
-  #   };
-  # };
-
   # Hardware
   hardware = {
     opengl = {
@@ -61,6 +53,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    weston
     waypaper
     swaybg
     pipewire
@@ -98,26 +91,4 @@
     adwaita-qt
     adwaita-qt6
   ];
-
-  # environment.sessionVariables = {
-  #   # POLKIT_AUTH_AGENT =
-  #   #   "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-  #   # GSETTINGS_SCHEMA_DIR =
-  #   #   "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
-  #   # LIBVA_DRIVER_NAME = "nvidia";
-  #   XDG_SESSION_TYPE = "wayland";
-  #   # GBM_BACKEND = "nvidia-drm";
-  #   # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  #   WLR_NO_HARDWARE_CURSORS = "1";
-  #   NIXOS_OZONE_WL = "1";
-  #   MOZ_ENABLE_WAYLAND = "1";
-  #   SDL_VIDEODRIVER = "wayland";
-  #   _JAVA_AWT_WM_NONREPARENTING = "1";
-  #   CLUTTER_BACKEND = "wayland";
-  #   WLR_RENDERER = "vulkan";
-  #   XDG_CURRENT_DESKTOP = "Hyprland";
-  #   XDG_SESSION_DESKTOP = "Hyprland";
-  #   GTK_USE_PORTAL = "1";
-  #   NIXOS_XDG_OPEN_USE_PORTAL = "1";
-  # };
 }
